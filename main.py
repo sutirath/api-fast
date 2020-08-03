b0334c5e5ccee8ed834ed9d11a265506aa9f8c8b
sutirath <39222262+sutirath@users.noreply.github.com>
2020-08-03 21:18:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,8 @@
-#import libraly
-from fastapi import FastAPI , File
-#from pydub import AudioSegment
-#import numpy as np
-#import librosa
-#import librosa.display
-#from tensorflow.keras.models import  load_model
+from fastapi import FastAPI, File, UploadFile
 
-
-
-names = ['bad','enemy','good','queen']
-model = load_model('model.h5')
-model.summary()
 app = FastAPI()
-
-#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
-
-@app.post("/")
-async def main():
-    return {"file_size"}
 
 
 @app.post("/files/")
-async def create_file(file: bytes = File(...)):
-    A = AudioSegment.from_file(file)
-    test=A
-    test = np.array(test.get_array_of_samples(), np.float32)
-    test=librosa.feature.mfcc(test)
-    sound = test[:,:939]
-    classi = names[model.predict(sound[None,:,:,None]).argmax()]
-    acc = model.predict(sound[None,:,:,None]).max()*100
-    
-    return {
-        "class":classi,
-        "acc":acc
-    }
-
+async def main(file: bytes = File(...)):
+    return {"file_size": len(file)}
