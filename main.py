a39e8197a8d4fc616381271771ce8822e6044f97
sutirath <39222262+sutirath@users.noreply.github.com>
2020-08-03 21:44:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
-#from pydub import AudioSegment
-#import numpy as np
-#import librosa
-#import librosa.display
-#from tensorflow.keras.models import  load_model
+from pydub import AudioSegment
+import numpy as np
+import librosa
+import librosa.display
+from tensorflow.keras.models import  load_model
 
 names = ['bad','enemy','good','queen']
 model = load_model('model.h5')
