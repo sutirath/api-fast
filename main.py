3b3d254e8791527f65f6f88d3159288605ba0de6
sutirath <39222262+sutirath@users.noreply.github.com>
2020-08-03 21:22:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 
 
+@app.post("/")
+def hello():
+    return {"Hello World"}
+
 @app.post("/files/")
 async def main(file: bytes = File(...)):
     return {"file_size": len(file)}
