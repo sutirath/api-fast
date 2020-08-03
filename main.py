d60d942280ffe0b6ff7c23ef2d4d55aaa05a5d57
sutirath <39222262+sutirath@users.noreply.github.com>
2020-08-03 21:28:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI, File, UploadFile
+from pydub import AudioSegment
+import numpy as np
+import librosa
+import librosa.display
+from tensorflow.keras.models import  load_model
 
+names = ['bad','enemy','good','queen']
+model = load_model('model.h5')
 app = FastAPI()
-
 
 @app.get("/")
 def hello():
@@ -9,4 +15,15 @@ def hello():
 
 @app.post("/files/")
 async def main(file: bytes = File(...)):
-    return {"file_size": len(file)}
+    A = AudioSegment.from_file(file)
+    test=A
+    test = np.array(test.get_array_of_samples(), np.float32)
+    test=librosa.feature.mfcc(test)
+    sound = test[:,:939]
+    classi = names[model.predict(sound[None,:,:,None]).argmax()]
+    acc = model.predict(sound[None,:,:,None]).max()*100
+    
+    return {
+        "class":classi,
+        "acc":acc
+    }
